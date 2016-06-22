c568fb6be8eb10045422ad3ac196d032bd37629a
vhavhugo <vhavhugo@gmail.com>
2016-06-21 22:48:38 -0300

    Primeiro Programa

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+    echo "Hoje é dia" . date ('d/m/Y');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title></title>
+    </head>
+    <body>
+        
+    </body>
+</html>
