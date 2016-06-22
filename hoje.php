2449f402a153aec21b6c79bde0e1bd89fb5a3811
vhavhugo <vhavhugo@gmail.com>
2016-06-21 23:13:19 -0300

    data e hora

---
+++ hoje.php
@@ -0,0 +1,15 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title>Dia <?php echo date('d'); ?></title>
+    </head>
+    <body>
+        <h1>Estamos em <?php echo date('Y'); ?></h1>
+        
+    </body>
+</html>
