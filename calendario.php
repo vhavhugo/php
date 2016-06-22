5d0d995e15c2ad975e04ba8c68407a67189535b3
vhavhugo <vhavhugo@gmail.com>
2016-06-21 23:20:50 -0300

    inserir function calendario

--- calendario.php
+++ calendario.php
@@ -12,6 +12,20 @@
         </tr>
         ";
     }
+
+    function calendario(){
+        $dia = 1;
+        $semana = array();
+        while($dia <= 31){
+            array_push($semana,$dia);
+
+            if(count($semana) == 7){
+                linha($semana);
+                $semana = array();
+            }
+            $dia++;
+        }
+      }
 ?>
 
 <!DOCTYPE html>
